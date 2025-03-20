880307af958e7527638b4cf570f6723024a344aa
B1M0r <tapelkabro@gmail.com>
2025-03-20 22:36:26 +0300

    sonar cloud

--- src/main/java/com/example/store/model/Product.java
+++ src/main/java/com/example/store/model/Product.java
@@ -1,6 +1,5 @@
 package com.example.store.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
